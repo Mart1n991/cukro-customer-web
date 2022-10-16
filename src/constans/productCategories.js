758794a8c658b7223d7325329d3fc22068484555
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-10-16 22:28:31 +0200

    Created getProductList function to fetch products according to related category. For fetch data used react router dom, loader properties

---
+++ src/constans/productCategories.js
@@ -0,0 +1,3 @@
+export const cakesCategoryId = "6217af9b4c49a4266b3007ad";
+export const smallCakesCategoryId = "6217af354c49a4266b3007ac";
+export const specialitesCategoryId = "6217af9b4c49a4266b3007ab";
