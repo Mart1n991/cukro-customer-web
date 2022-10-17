9c06f72529717596e2ff6ae274abf99d31afaef7
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-10-17 11:33:21 +0200

    Created error handling when fetch data

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,7 @@ import Order from "../pages/order/Order";
 import Home from "../pages/home/Home";
 import { getProductList } from "../api/requests";
 import { cakesCategoryId, smallCakesCategoryId, specialitesCategoryId } from "../constans/productCategories";
+import Error from "../components/error/Error";
 
 export const router = createBrowserRouter([
   {
@@ -16,16 +17,19 @@ export const router = createBrowserRouter([
     path: "/torty",
     element: <Cakes />,
     loader: () => getProductList(cakesCategoryId),
+    errorElement: <Error />,
   },
   {
     path: "/kolace",
     element: <SmallCakes />,
     loader: () => getProductList(smallCakesCategoryId),
+    errorElement: <Error />,
   },
   {
     path: "/speciality",
     element: <Specialties />,
     loader: () => getProductList(specialitesCategoryId),
+    errorElement: <Error />,
   },
   {
     path: "/objednavka",
