376b14dd274a8e81a80025552caece78bfcf8157
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-10-17 12:46:09 +0200

    Created card for cake product. And displayed it to user

---
+++ src/components/cakeCard/CakeCard.js
@@ -0,0 +1,46 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, Button, CardActionArea, CardActions } from "@mui/material";
+import basicCakePicture from "../../assets/Torty_1.jpg";
+
+const CakeCard = ({ name, price, weight }) => {
+  return (
+    <Card sx={{ width: 300, margin: 2 }}>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={basicCakePicture} alt="green iguana" />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {name}
+          </Typography>
+          <Box>
+            <Box sx={{ display: "flex" }}>
+              <Typography variant="body2" color="text.secondary" width={80}>
+                Hmotnosť:
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {weight} gramov
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex" }}>
+              <Typography variant="body2" color="text.secondary" width={80}>
+                Cena:
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {price}€
+              </Typography>
+            </Box>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" variant="contained">
+          Pozrieť tortu
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CakeCard;
--- src/pages/cakes/Cakes.js
+++ src/pages/cakes/Cakes.js
@@ -1,10 +1,20 @@
-import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import { useLoaderData } from "react-router-dom";
+import CakeCard from "../../components/cakeCard/CakeCard";
 import Layout from "../../components/layout/Layout";
 
-const Cakes = () => (
-  <Layout>
-    <Typography>TORTY</Typography>
-  </Layout>
-);
+const Cakes = () => {
+  const { data } = useLoaderData();
+
+  return (
+    <Layout>
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        {data.map(({ _id, name, price, weight }) => (
+          <CakeCard key={_id} name={name} price={price} weight={weight} />
+        ))}
+      </Box>
+    </Layout>
+  );
+};
 
 export default Cakes;
