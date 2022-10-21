6921d6d93016a8169fcc0c22b19259ad41ba826e
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-10-21 08:48:02 +0200

    created basic cake detail page with dummy data. for testing is now on home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,11 @@
 import { Typography } from "@mui/material";
+import CakeDetail from "../../components/cakeDetail/CakeDetail";
 
-const Home = () => <Typography>HOME PAGE</Typography>;
+const Home = () => (
+  <>
+    <Typography>HOME PAGE</Typography>
+    <CakeDetail />
+  </>
+);
 
 export default Home;
